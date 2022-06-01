ee3d91953e237014721c19ea273585e7cba181af
Tacio <taciosfer@gmail.com>
2022-05-31 21:37:53 -0300

    Missing Annotations!

--- blogpessoal/src/main/java/com/generation/blogpessoal/controller/TemaController.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/controller/TemaController.java
@@ -1,6 +1,7 @@
 package com.generation.blogpessoal.controller;
 
 import java.util.List;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -36,11 +37,11 @@ public class TemaController {
 		return ResponseEntity.ok(repository.findAllByDescricaoContainingIgnoreCase(nome));
 	}
 	@PostMapping
-	public ResponseEntity<Tema> post (@RequestBody Tema tema) {
+	public ResponseEntity<Tema> post (@Valid @RequestBody Tema tema) {
 		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(tema));
 	}
 	@PutMapping
-	public ResponseEntity<Tema> put (@RequestBody Tema tema) {
+	public ResponseEntity<Tema> put (@Valid @RequestBody Tema tema) {
 		return ResponseEntity.ok(repository.save(tema));
 	}
 	@DeleteMapping("/{id}")
