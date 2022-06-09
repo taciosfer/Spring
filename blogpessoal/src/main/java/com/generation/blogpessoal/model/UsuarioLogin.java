5b2bd1a15bcfda7864583bc8ebfc51c56887d121
Tacio <taciosfer@gmail.com>
2022-06-09 00:09:17 -0300

    Ajustes Security

--- blogpessoal/src/main/java/com/generation/blogpessoal/model/UsuarioLogin.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/model/UsuarioLogin.java
@@ -1,6 +1,16 @@
 package com.generation.blogpessoal.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class UsuarioLogin {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
 	
 	private String nome;
 	
