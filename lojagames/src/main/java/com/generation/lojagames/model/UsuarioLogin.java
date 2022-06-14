dd5cb85f4d0a9d263c7ee631500bc34f41ec84f3
Tacio <taciosfer@gmail.com>
2022-06-14 03:22:27 -0300

    Adição @Entity em Model

--- lojagames/src/main/java/com/generation/lojagames/model/UsuarioLogin.java
+++ lojagames/src/main/java/com/generation/lojagames/model/UsuarioLogin.java
@@ -1,9 +1,11 @@
 package com.generation.lojagames.model;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Entity
 public class UsuarioLogin {
 
 	@Id
